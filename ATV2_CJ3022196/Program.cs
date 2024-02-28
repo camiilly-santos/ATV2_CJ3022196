5396ee583e78f4f6b2f847e585e6eed4107bd9dd
CJ3022196 <CJ3022196@LAB01-PC39>
2024-02-28 08:56:48 -0300

    Adicionar arquivos de projeto.

---
+++ ATV2_CJ3022196/Program.cs
@@ -0,0 +1,17 @@
+﻿float salario;
+
+//Leitura do salário
+Console.WriteLine("Entre com um salário");
+
+
+salario = float.Parse (Console.ReadLine());
+
+salario = salario + (float) (25 / 100) * salario;
+
+Console.WriteLine("O novo salário é R$ {0} ", salario);
+
+
+
+
+
+
